3bb1d7310e43b13836a045444635a8a66b2ea676
stualyttle <stualyttle@users.noreply.github.com>
2025-07-27 19:03:04 +0200

    Update action bar handling in StaffCommand and upgrade LyttleUtils dependency to version 1.1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     api(libs.org.xerial.sqlite.jdbc)
     compileOnly(libs.io.papermc.paper.paper.api)
     compileOnly(libs.net.luckperms.api)
-    implementation("com.lyttledev:lyttleutils:1.1.5-BETA.006")
+    implementation("com.lyttledev:lyttleutils:1.1.7")
 }
 
 group = "com.lyttledev"
